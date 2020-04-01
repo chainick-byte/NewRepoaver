c0509dcb997fa919b92a59c4d8c76f4b0b076817
chainick-byte <igorrepyakh@gmail.com>
2020-04-01 15:08:40 +0100

    comiit es un commit

---
+++ Probando_HolaMundo/Probando_HolaMundo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Probando_HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /**
+         * voy a probar hacer un comit push*
+         **/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hola MUndo";
+            //hola mundo
+        }
+    }
+}
